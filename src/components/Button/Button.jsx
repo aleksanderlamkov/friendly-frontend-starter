64a52036ddfb3acdd6f0fc35fe2821c848712466
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2025-07-16 15:40:02 +0300

    refactor(Button): изменил имена вспомогательных переменных на более подходящие

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,16 +2,16 @@ import './Button.scss'
 import clsx from 'clsx'
 
 export default (props) => {
-  const { className, type = 'button', href, children, ...otherProps } = props
+  const { className, type = 'button', href, children } = props
 
   const isLink = href !== undefined
   const Component = isLink ? 'a' : 'button'
-  const linkProps = { href }
-  const buttonProps = { type }
-  const specificProps = isLink ? linkProps : buttonProps
+  const linkAttributes = { href }
+  const buttonAttributes = { type }
+  const attributesByTag = isLink ? linkAttributes : buttonAttributes
 
   return (
-    <Component className={clsx('button', className)} {...specificProps}>
+    <Component className={clsx('button', className)} {...attributesByTag}>
       {children}
     </Component>
   )
